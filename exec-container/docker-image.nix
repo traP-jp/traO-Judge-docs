b31ffe8629518cb41af04b0bf15330d9c34611dc
mtaku3 <m.taku3.1222@gmail.com>
2024-10-20 13:18:08 +0900

    feat: add compilers directory

--- exec-container/docker-image.nix
+++ exec-container/docker-image.nix
@@ -1,8 +1,9 @@
 {pkgs}: let
   interpreters = import ./interpreters {inherit pkgs;};
+  compilers = import ./compilers {inherit pkgs;};
 in
   pkgs.dockerTools.buildImage {
     name = "exec-container";
 
-    copyToRoot = interpreters.all;
+    copyToRoot = interpreters.all ++ compilers.all;
   }
