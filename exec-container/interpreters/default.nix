aad6c6c05c4d37196d54de9eeb5581cbb49a430d
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2024-11-08 15:32:31 +0900

    Add sed

--- exec-container/interpreters/default.nix
+++ exec-container/interpreters/default.nix
@@ -1,10 +1,12 @@
 {pkgs}: let
   python = import ./python {inherit pkgs;};
   bash = import ./bash {inherit pkgs;};
+  sed = import ./sed {inherit pkgs;};
 in {
   all = [
     python.python311
     python.python312
     bash
+    sed
   ];
 }
