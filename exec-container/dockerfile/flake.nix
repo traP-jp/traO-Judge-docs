3c948782151d2a7d1d1589d7c8c07f45c6bfe689
comavius <comavius@proton.me>
2024-11-10 02:24:53 +0900

    ✨ debug without docker

---
+++ exec-container/dockerfile/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    container-env.url = "../";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    container-env,
+    ...
+  }: 
+    flake-utils.lib.eachDefaultSystem (system: let 
+      pkgs = import nixpkgs { inherit system; };
+    in
+      {
+        packages.default = pkgs.dockerTools.buildImage {
+          name = "exec-container";
+          copyToRoot = [
+            container-env.packages.${system}.default
+          ];
+        };
+      }
+    );
+}
--- exec-container/flake.nix
+++ exec-container/flake.nix
@@ -42,7 +42,19 @@
         ];
       };
     in {
-      packages.default = import ./docker-image.nix {inherit pkgs;};
+      packages.default = let
+        interpreters = import ./interpreters {inherit pkgs;};
+        compilers = import ./compilers {inherit pkgs;};
+        tools = import ./tools {inherit pkgs;};
+      in
+        pkgs.symlinkJoin {
+          name = "exec-container-env";
+          paths = [
+            interpreters.all
+            compilers.all
+            tools.all
+          ];
+        };
       formatter = pkgs.alejandra;
     });
 }
