d1a95cca86c499adf1c6f95839409e1eefa6a21c
comavius <comavius@proton.me>
2024-11-28 01:22:02 +0900

    ✨ koka lang compiler

--- exec-container/compilers/default.nix
+++ exec-container/compilers/default.nix
@@ -2,10 +2,12 @@
   golang = import ./golang {inherit pkgs;};
   brainfuck = import ./brainfuck {inherit pkgs;};
   rust = import ./rust {inherit pkgs;};
+  koka = import ./koka {inherit pkgs;};
 in {
   all = [
     golang
     brainfuck
     rust
+    koka
   ];
 }
