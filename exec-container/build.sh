ee761fd4ed1b45ca37c1e12eed049796aff39ac0
pasmophobia <l@leonmatsuistroemsvik.com>
2024-12-12 17:34:48 +0900

    feat: add interpreter swi-prolog

--- exec-container/build.sh
+++ exec-container/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -eux
+
 touch result
 docker build -t trao-nix .
 docker volume create trao-nix_cache
-docker run -v ./result:/result -v trao-nix_cache:/nix/store trao-nix
+docker run -v ./result:/result -v trao-nix_cache:/nix/store --platform=linux/amd64 trao-nix
---
+++ exec-container/interpreters/prolog/default.nix
@@ -0,0 +1,4 @@
+{pkgs, ...}: let
+    myProlog = pkgs.swi-prolog;
+in
+    pkgs.writeShellScriptBin "swipl" "exec ${myProlog}/bin/swipl $@"
