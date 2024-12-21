68726c0a6892073a2257adbd2e24eee999f93e31
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2024-12-21 19:52:32 +0900

    ✨ languageSettingsをDockerでできるように

---
+++ exec-container/docker/languageSettings-in-docker.sh
@@ -0,0 +1,16 @@
+#!/root/.nix-profile/bin/bash
+
+# コンテナビルド
+if ! nix build --extra-experimental-features nix-command --extra-experimental-features flakes .#languageSettings
+then
+  exit 1
+fi
+
+# /resultに書き込み権限を付与
+chmod u+w /result
+
+# ビルド結果をコピー
+realpath /workspace/result | xargs -I {} cp {} /result
+
+# 読み取り専用に戻す
+chmod u-w /result
