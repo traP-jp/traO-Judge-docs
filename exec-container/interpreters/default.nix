82b48df9ca9b1d4ce01840a2ad1ee6a4b30c7136
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2024-12-23 22:27:00 +0900

    🎨 これから追加する言語をコメントで追加

--- exec-container/interpreters/default.nix
+++ exec-container/interpreters/default.nix
@@ -37,5 +37,18 @@ in {
     python.python311.traojudge
     python.python312.traojudge
     python.python312-quantum.traojudge
+    #bash.traojudge
+    #sed.traojudge
+    #node.traojudge
+    #seed7.traojudge
+    #pwsh.traojudge
+    ## sagemath.traojudge
+    #awk.traojudge
+    #whitespace.traojudge
+    #unison.traojudge
+    #zsh.traojudge
+    #lua.traojudge
+    #r.traojudge
+    #prolog.traojudge
   ];
 }
