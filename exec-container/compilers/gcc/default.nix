92386afdf09c8d9c825a54f2f9842ed8f49b4a69
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-13 14:41:13 +0900

    ✨ g++とgccのコンパイルコマンド

--- exec-container/compilers/gcc/default.nix
+++ exec-container/compilers/gcc/default.nix
@@ -2,3 +2,15 @@
   myGcc = pkgs.gcc;
 in
   pkgs.writeShellScriptBin "gcc" "exec ${myGcc}/bin/gcc $@"
+  // {
+    traojudge = {
+      languages = [
+        {
+          binName = "gcc";
+          compile = "${myClang}/bin/gcc -o $OUT $SRC";
+          name = "C";
+          run = "$OUT";
+        }
+      ];
+    };
+  }
