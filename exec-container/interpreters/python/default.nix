a93542d82a89821584e4a15c5eda58712f200311
comavius <comavius@proton.me>
2025-04-04 04:12:33 +0900

    ✨ add python interpreter for traopy util

--- exec-container/interpreters/python/default.nix
+++ exec-container/interpreters/python/default.nix
@@ -2,4 +2,5 @@
   python311 = import ./3.11 {inherit pkgs;};
   python312 = import ./3.12 {inherit pkgs;};
   python312-quantum = import ./3.12-quantum {inherit pkgs;};
+  python312-traopy-util = import ./3.12-traopy-util {inherit pkgs;};
 }
